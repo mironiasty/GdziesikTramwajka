2940417b0b1bc31c19b3ea7b9d563270367b9d44
Miron <miron@live.com>
2017-04-20 20:03:50 +0200

    decode stops names

--- js/utils/TtssFetch.js
+++ js/utils/TtssFetch.js
@@ -1,10 +1,16 @@
 
-
+import { Html5Entities } from 'html-entities'
+const entities = new Html5Entities()
 
 export async function searchForStops(partialName) {
-  const response = await fetch('http://www.ttss.krakow.pl/internetservice/services/lookup/autocomplete/json?query=' + partialName)
-  const stops = await response.json()
-  stops.shift()
-  return stops
+    const response = await fetch('http://www.ttss.krakow.pl/internetservice/services/lookup/autocomplete/json?query=' + partialName)
+    const stops = await response.json()
+    stops.shift()
+    console.log('stops', stops)
+    return stops.map(i => ({id: i.id,name: decode(i.name)}))
+}
+
+function decode( name ) {
+    return entities.decode(name);
 }
 
